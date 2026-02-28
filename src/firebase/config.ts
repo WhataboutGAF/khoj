2101d1e500cc395bc2b0918589df6fbb551a739d
Glorified DUCK <glorifiedduck@gmail.com>
2026-02-28 10:11:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9398391734-6bb9f",
+  "appId": "1:254226263585:web:855ce8f107bcf4ef9ffd60",
+  "apiKey": "AIzaSyCzgeuo0hSfjo9Ya8NgqzWX32SHvv0onNc",
+  "authDomain": "studio-9398391734-6bb9f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "254226263585"
+};
